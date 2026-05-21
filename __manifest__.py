e0c193728c437539808008ddfa639c7a4fa98757
Michele Di Croce <dicroce.m@stesi.eu>
2026-05-21 12:54:07 +0200

    [19.0][FIX] Deep migration to Odoo 19

--- __manifest__.py
+++ __manifest__.py
@@ -1,49 +1,20 @@
 # STeSI Consulting - Michele Di Croce
 # License OPL-1 (https://www.odoo.com/documentation/user/19.0/legal/licenses/licenses.html).
-#############################################################################
-#
-#    Cybrosys Technologies Pvt. Ltd.
-#
-#    Copyright (C) 2023-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
-#    Author: Cybrosys Techno Solutions(<https://www.cybrosys.com>)
-#
-#    You can modify it under the terms of the GNU LESSER
-#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
-#
-#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
-#    (LGPL v3) along with this program.
-#    If not, see <http://www.gnu.org/licenses/>.
-#
-#############################################################################
-
+# Portal Product Availability — Cybrosys Technologies adaptation
 {
     'name': "Portal Product Availability",
     'version': '19.0.1.0.0',
     'category': 'Sales',
-    'summary': """Portal Product Availability""",
-    'description': """Portal Users Can Check The Availability of Products""",
-    'author': "Cybrosys Techno Solutions",
-    'company': "Cybrosys Techno Solutions",
-    'maintainer': 'Cybrosys Techno Solutions',
-    'website': "https://www.cybrosys.com",
-    'images': ['static/description/banner.png'],
-    'license': 'LGPL-3',
+    'summary': "Portal users can check product availability",
+    'license': 'OPL-1',
+    'author': "STeSI Consulting",
+    'website': "https://github.com/ingegniamo/portal_stock_check",
     'depends': ['portal', 'sale_management', 'stock', 'contacts'],
-
-    'data': [
-        'views/portal_inherited.xml',
-    ],
-
+    'data': ['views/portal_inherited.xml'],
     'assets': {
         'web.assets_frontend': [
             'portal_stock_check/static/src/js/search_products.js',
             'portal_stock_check/static/src/xml/product_list.xml'
         ],
     },
-    'auto_install': False,
 }
